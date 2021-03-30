e44e464be2f57891de0a6531a5f126304b689a60
rileysleight <510479@chrome.escg.ac.uk>
2021-03-30 12:14:05 +0100

    Initial commit

---
+++ Assets/Scripts/TriggerSound.cs
@@ -0,0 +1,32 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+ using UnityEngine.EventSystems;
+
+public class TriggerSound : MonoBehaviour, ISelectHandler , IPointerEnterHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //print("entered");
+        AudioManager.instance.Play("MenuPointerEnter");
+    }
+
+    public void OnSelect( BaseEventData eventData )
+    {
+        //print("selected");
+        AudioManager.instance.Play("MenuSelect");
+    }
+}
