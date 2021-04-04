8560b67af29e7c818505373b7d12472e2a55cfaf
rileysleight <510479@chrome.escg.ac.uk>
2021-04-04 13:55:52 +0100

    Navigation Works :)

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,18 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
-public class MainMenu : MonoBehaviour {
+public class MainMenu : MonoBehaviour 
+{
 
- public void PlayGame () 
- {
-    SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
- }
- 
- public void QuitGame ()
- {
-    Debug.Log ("QUIT!");
-    Application.Quit();
- }
+   public GameObject menuFirst, optionsFirst, instructionsFirst;
+
+   public void PlayGame () 
+   {
+      SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+   }
+   
+   public void QuitGame ()
+   {
+      Debug.Log ("QUIT!");
+      Application.Quit();
+   }
+
+   public void OptionsNav()
+   {
+      EventSystem.current.SetSelectedGameObject(null);
+      EventSystem.current.SetSelectedGameObject(optionsFirst);
+   }
+
+   public void InstructionsNav()
+   {
+      EventSystem.current.SetSelectedGameObject(null);
+      EventSystem.current.SetSelectedGameObject(instructionsFirst);
+   }
+   
+   public void MenuNav()
+   {
+      EventSystem.current.SetSelectedGameObject(null);
+      EventSystem.current.SetSelectedGameObject(menuFirst);
+   }
   
 }
